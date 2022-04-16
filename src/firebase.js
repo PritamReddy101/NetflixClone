f519b9a7565174529fb867496278d715ad451a93
Your Name <pritam.nandikonda@gmail.com>
2022-04-16 14:58:17 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBn8zTFVecKC7KrbyLsUCXEE-VjfrtZCws",
+    authDomain: "netflix-clone-d2060.firebaseapp.com",
+    projectId: "netflix-clone-d2060",
+    storageBucket: "netflix-clone-d2060.appspot.com",
+    messagingSenderId: "908662891202",
+    appId: "1:908662891202:web:40706661a419d0688f515b"
+  };
